ff1b93d34e979082049ef9eb24c6b09005863eeb
pradip <pradipawasthi701@gmail.com>
2021-04-02 14:26:04 +0530

    updated version n minor ui

--- awesome-calendar/src/main/java/com/archit/calendardaterangepicker/customviews/DateRangeCalendarView.kt
+++ awesome-calendar/src/main/java/com/archit/calendardaterangepicker/customviews/DateRangeCalendarView.kt
@@ -47,8 +47,6 @@ class DateRangeCalendarView : LinearLayout, DateRangeCalendarViewApi {
         calendarStyleAttr = CalendarStyleAttrImpl(context, attrs)
         val layoutInflater = LayoutInflater.from(context)
         layoutInflater.inflate(layout.layout_calendar_container, this, true)
-        val rlHeaderCalendar = findViewById<RelativeLayout>(R.id.rlHeaderCalendar)
-        rlHeaderCalendar.background = calendarStyleAttr.headerBg
         tvYearTitle = findViewById(R.id.tvYearTitle)
         tvYearTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, calendarStyleAttr.textSizeTitle)
         imgVNavLeft = findViewById(R.id.imgVNavLeft)
